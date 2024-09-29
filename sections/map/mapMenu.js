63d35dfaecf7da808e764fc406a201f243ded2a2
Asasay <blackjewrobot@gmail.com>
2024-09-30 01:11:58 +0300

    map menu rework and sorted css declarations

--- sections/map/mapMenu.js
+++ sections/map/mapMenu.js
@@ -2,7 +2,9 @@ export const attachMapMenuHandlers = () => {
   const dropdown = document.querySelector("div[class^=map__dropdown]");
   dropdown.addEventListener("click", dropdownHandler(dropdown));
 
-  const expandables = document.querySelectorAll("li[class^=map__nav-item--expandable]");
+  const expandables = document.querySelectorAll(
+    "li[class^=map__nav-item--expandable]",
+  );
   for (const elem of expandables) {
     elem.addEventListener("click", expandableHanlder(expandables));
   }
@@ -10,9 +12,17 @@ export const attachMapMenuHandlers = () => {
 
 function dropdownHandler(dropdown) {
   return function (e) {
-    if (this == e.target || dropdown.querySelector(".map__dropdown-arrow").contains(e.target)) {
-      if (this.className == "map__dropdown--closed") this.className = "map__dropdown--opened";
-      else this.className = "map__dropdown--closed";
+    if (
+      this == e.target ||
+      dropdown.querySelector(".map__dropdown-arrow").contains(e.target)
+    ) {
+      if (this.className == "map__dropdown--closed") {
+        this.className = "map__dropdown--opened";
+        document.querySelector("object.map__map-img").style.opacity = "10%";
+      } else {
+        this.className = "map__dropdown--closed";
+        document.querySelector("object.map__map-img").style.opacity = "100%";
+      }
     }
   };
 }
